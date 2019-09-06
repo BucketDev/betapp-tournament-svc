cb8756f2882d6ec6cd0c5fed975c4b7c53921c74
vanfigo <rodrigo.loyola@live.com>
2019-09-06 16:58:02 -0500

    first commit

---
+++ src/main/java/com/bucketdev/betapptournamentsvc/BetappTournamentSvcApplication.java
@@ -0,0 +1,13 @@
+package com.bucketdev.betapptournamentsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetappTournamentSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetappTournamentSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bucketdev/betapptournamentsvc/BetappTournamentSvcApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bucketdev.betapptournamentsvc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BetappTournamentSvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
